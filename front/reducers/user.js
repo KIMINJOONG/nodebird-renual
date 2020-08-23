adedc2aa3d4983275bdeb201de4c4252ae5dd0b4
KIMINJOONG <ijboym216@gmail.com>
2020-08-23 11:59:12 +0900

    4-1 redux thunk이해하기

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -6,15 +6,59 @@ const initialState = {
 };
 
 export const loginAction = (data) => {
+    return (dispatch, getState) => {
+        const state = getState();
+
+        dispatch(loginRequestAction());
+        axios
+            .post("/api/login")
+            .then(() => {
+                dispatch(loginSuccessAction());
+            })
+            .catch(() => {
+                dispatch(loginFailureAction());
+            });
+    };
+};
+
+export const loginRequestAction = (data) => {
     return {
-        type: "LOG_IN",
+        type: "LOG_IN_REQUEST",
         data,
     };
 };
 
-export const logoutAction = (data) => {
+export const loginSuccessAction = (data) => {
     return {
-        type: "LOG_OUT",
+        type: "LOG_IN_SUCCESS",
+        data,
+    };
+};
+
+export const loginFailureAction = (data) => {
+    return {
+        type: "LOG_IN_FAILURE",
+        data,
+    };
+};
+
+export const logoutRequestAction = (data) => {
+    return {
+        type: "LOG_OUT_REQUEST",
+        data,
+    };
+};
+
+export const logoutSuccessAction = (data) => {
+    return {
+        type: "LOG_OUT_SUCCESS",
+        data,
+    };
+};
+
+export const logoutFailureAction = (data) => {
+    return {
+        type: "LOG_OUT_FAILURE",
         data,
     };
 };
