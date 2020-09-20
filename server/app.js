10f164d56f45d3ba55cf9e3af55c7c3bab17f7d3
KIMINJOONG <ijboym216@gmail.com>
2020-09-20 21:48:09 +0900

    5-1. 노드로 서버 구동하기

---
+++ server/app.js
@@ -0,0 +1,10 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+    res.end("hello node");
+});
+
+server.listen(3065, () => {
+    console.log("실행중");
+});
